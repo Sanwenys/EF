3040aabadb78d1d0684e5f13c90893fc8ee39eb5
Sany <96656150+Sanwenys@users.noreply.github.com>
2022-12-04 20:10:00 +0800

    【日常更新】

--- EF/EF/EF.cs
+++ EF/EF/EF.cs
@@ -1,7 +1,36 @@
-﻿namespace EF
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace EF
 {
     public class EF
     {
-        private class Db : 
+        private class Db : DbContext
+        {
+            public virtual DbSet<ComEnity> ComEnities { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuiler)
+            {
+                optionsBuiler.UseSqlServer(@"Server=(local);Database=Nutsheel;Trusted_Connection=True");
+            }
+
+            protected override void OnModelCreating(ModelBuilder model)
+            {
+                model.Entity<ComEnity>().ToTable("ComEnity").HasKey(c => c.Id);
+            }
+
+            public Db(DbContextOptions<Db> options) : base(options)
+            {
+
+            }
+        }
+
+
+        public class ComEnity
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Code { get; set; }
+        }
     }
 }
